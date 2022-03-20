e12716d13cca864493405917608b797ddefbcedd
Brimaz <brimaz@oreskos.com>
2022-03-20 01:25:42 -0400

    Add view stubs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ExploreComponent, PageNotFoundComponent, PolygonManagementComponent } from '@views';
+import { ExploreComponent, PageNotFoundComponent, PolygonManagementComponent, ProfileComponent, SettingsComponent } from '@views';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/explore' },
   { path: 'explore', component: ExploreComponent },
   { path: 'polygons', component: PolygonManagementComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'settings', component: SettingsComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
